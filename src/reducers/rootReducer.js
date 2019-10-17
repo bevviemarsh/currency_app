ea574e0b29992d044c4ef43b682ac8c6ac19b808
Paweł Marciniak <pablomchod@gmail.com>
2019-10-17 05:48:46 +0200

    update of store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,7 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initState = {
-  conversion: "",
-  transactions: []
+  conversion: ""
 };
 
 const rootReducer = (state = initState, action) => {
