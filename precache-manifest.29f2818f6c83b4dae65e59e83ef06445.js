2b16ec3890036f657fb12df4476627a3071cdd87
Paweł Marciniak <pablomchod@gmail.com>
2019-10-17 06:54:03 +0200

    Updates

---
+++ precache-manifest.29f2818f6c83b4dae65e59e83ef06445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2e9ed9490e3d56186f55b98c1953a1",
+    "url": "/currency_app/index.html"
+  },
+  {
+    "revision": "76fcfdfbfb573fe611ea",
+    "url": "/currency_app/static/css/main.d7433fe4.chunk.css"
+  },
+  {
+    "revision": "7d62dedcf3d282ccc27a",
+    "url": "/currency_app/static/js/2.3074104b.chunk.js"
+  },
+  {
+    "revision": "76fcfdfbfb573fe611ea",
+    "url": "/currency_app/static/js/main.3638c592.chunk.js"
+  },
+  {
+    "revision": "c016da4e40f4c6126bac",
+    "url": "/currency_app/static/js/runtime-main.3a8b2b9a.js"
+  }
+]);
